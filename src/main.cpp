3ebb58b7716065def8a408bc4dd46f4f64d2878a
IQUER_SAENZ <206500499+DennysSaenz123@users.noreply.github.com>
2026-01-15 05:51:32 +0000

    Added keypad functions to change background color.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_core.h>
+#include <bn_keypad.h>
+
 
 int main(){
     bn::core::init();
     bn::backdrop::set_color(bn::color(20, 20, 0));
+    
+
     while(true) {
+        if(bn::keypad::a_held()){
+        bn::backdrop::set_color(bn::color(31,21, 22));
+    } 
+        if (bn::keypad::a_released()){
+        bn::backdrop::set_color(bn::color(20, 20, 0));
+    }
+    if(bn::keypad::b_pressed()){
+        bn::backdrop::set_color(bn::color(24,31, 22));
+    } 
+    {
+        /* code */
+    }
+    
+
+
     bn::core::update();
 }
 
